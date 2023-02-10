18f50f49dc7bec3a136fe3b238d737851a8fc84e
Fernando Catu <fercatuo@gmail.com>
2023-02-09 20:57:51 -0600

    validacones para mejorar el juego

--- Fundamentos de Python/main.py
+++ Fundamentos de Python/main.py
@@ -3,6 +3,10 @@ import random
 #Crear tupla
 options = ('piedra', 'papel', 'tijera')
 
+# Cuantas veces ha ganado
+computer_wins = 0
+user_wins = 0
+
 rounds = 1
 
 while True: 
@@ -11,11 +15,18 @@ while True:
     print('ROUNDS: ', rounds)
     print('*' * 10)
 
+    print('computer_wins', computer_wins)
+    print('user_wins', user_wins)
+
     user_option = input('Piedra, papel o tijera => ')
     user_option = user_option.lower()
+
+    rounds += 1 
+
     # Verificar si un elemento está en una tupla
     if not user_option in options:
         print('Esa opcion no es valida')
+        continue
     # Seleccion aleatoria para computer
     computer_option = random.choice(options)
 
@@ -28,21 +39,32 @@ while True:
         if computer_option == 'tijera':
             print('Piedra gana a tijera')
             print('User ganó')
+            user_wins += 1
         else:
             print('Papel gana a piedra')
             print('Computer ganó')
+            computer_wins += 1
     elif user_option == 'papel':
         if computer_option == 'piedra':
             print('papel le gana a piedra')
             print('User ganó')
+            user_wins += 1
         else:
             print('tijera le gana a papel')
             print('computer ganó')
+            computer_wins += 1
     elif user_option == 'tijera':
         if computer_option == 'papel':
             print('tijera le gana a piedra')
             print('user ganó')
+            user_wins += 1
         else:
             print('piedra le gana a papel')
             print('computer ganó')
-    rounds += 1
+            computer_wins += 1
+    if computer_wins == 2:
+        print('El ganador es la computadora')
+        break
+    if computer_wins == 2:
+        print('El ganador es la computadora')
+        break
